d2f37a632bda8435b45445ff3c657f1f263e708b
Maria <mariachiquidq2002@gmail.com>
2021-06-28 21:35:00 -0400

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mysql = require('mysql');
-const myConnection = require ('express-myconnection');
+const myConnection = require('express-myconnection');
 
 const app = express();
 
 // importing routes
 const tareasRoutes = require('./routes/tarea');
-const { urlencoded } = require('express');
+const {
+    urlencoded
+} = require('express');
 
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views',path.join(__dirname, '/views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
-host:'localhost',
-user:'root',
-port:'3306',
-database:'listadetareas',
-},'single'));
-app.use(express.urlencoded({extended: false}));
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE || "listadetareas"
+}, 'single'));
+app.use(express.urlencoded({
+    extended: false
+}));
 
 // routes
 app.use('/', tareasRoutes);
